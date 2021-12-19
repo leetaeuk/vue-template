3026d68858c5c635fe16de701a369590e79ce3c4
leetaeuk <78855863+leetaeuk@users.noreply.github.com>
2021-12-19 11:55:39 +0900

    로딩바 컴포넌트 추가

---
+++ src/components/Loading.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-card color="primary" dark>
+        <v-card-text class="pt-3">
+            데이터를 처리중입니다...
+            <v-progress-linear
+                indeterminate
+                color="white"
+                class="mb-0"
+            ></v-progress-linear>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: "Loading",
+
+    props: {
+        isOpen: {type: Boolean, default: false},
+        params: {type: Object},
+    },
+    methods: {
+    },
+}
+</script>
