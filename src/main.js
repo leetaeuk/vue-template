cb1d40bf4baa4b1165c31466a1674a7bdac61f6b
leetaeuk <kindly007@gmail.com>
2021-12-27 08:41:58 +0900

    폰트 위치 변경

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
 import '@/plugins/vue-composition-api'
 import '@/styles/styles.scss'
 import Vue from 'vue'
+import cssVars from 'css-vars-ponyfill'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import store from './store'
-import common from '@/assets/js/common';
-import axios from "@/plugins/axios";
+import common from '@/assets/js/common'
+import axios from '@/plugins/axios'
 import calendar from '@/components/Calendar.vue'
 
 Vue.config.productionTip = false
 
-Vue.use(common);
-Vue.use(axios);
+Vue.use(common)
+Vue.use(axios)
 
-Vue.component("Calendar", calendar)
+cssVars()
+
+Vue.component('Calendar', calendar)
 
 new Vue({
   router,
